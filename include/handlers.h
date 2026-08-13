836fa48cdadd58194ab7c10d3050c9a0278617f2
Алексей <alescha03@yandex.ru>
2026-08-13 15:14:47 +0300

    Добавлен файл index.html и его обработка - сервер может открыть страницу приветствия

--- include/handlers.h
+++ include/handlers.h
@@ -1,8 +1,20 @@
 #pragma once
+
 #include "crow_all.h"
+#include <fstream>
+#include <string>
+#include <windows.h>
+
+std::string getExecutablePath() {
+    char buffer[MAX_PATH];
+    GetModuleFileName(NULL, buffer, MAX_PATH);
+    std::string path(buffer);
+    return path.substr(0, path.find_last_of("\\/") + 1);
+}
 
 template <typename AppType>
 void setupRoutes(AppType& app) {
+
     CROW_ROUTE(app, "/")([]() {
         return "Hello, World!";
     });
@@ -33,5 +45,39 @@ void setupRoutes(AppType& app) {
         crow::json::wvalue result;
         result["sum"] = sum;
         return crow::response(result);
+    });
+
+    CROW_ROUTE(app, "/files/<path>")
+    ([](const std::string& path) {
+
+        if (path.find("..") != std::string::npos) {
+            return crow::response(403, "Forbidden");
+        }
+
+        std::string base_path = getExecutablePath() + "..\\..\\static\\";
+	std::string full_path = base_path + path;
+	std::cout << "Looking for file: " << full_path << std::endl;
+        std::ifstream file(full_path, std::ios::binary);
+        if (!file.is_open()) {
+            return crow::response(404, "File not found");
+        }
+
+        std::string ext = path.substr(path.find_last_of('.') + 1);
+        std::string content_type;
+        if (ext == "html") content_type = "text/html";
+        else if (ext == "css") content_type = "text/css";
+        else if (ext == "js") content_type = "application/javascript";
+        else if (ext == "png") content_type = "image/png";
+        else if (ext == "jpg" || ext == "jpeg") content_type = "image/jpeg";
+        else if (ext == "svg") content_type = "image/svg+xml";
+        else content_type = "application/octet-stream";
+
+        std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+        crow::response res;
+        res.set_header("Content-Type", content_type);
+        res.code = 200;
+        res.body = content;
+        return res;
     });
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,23 @@
 #include "logger_middleware.h"
 
 int main() {
-    crow::App<LoggingMiddleware> app;
+    try {
+        crow::App<LoggingMiddleware> app;
 
-    setupRoutes(app);
+        setupRoutes(app);
 
-    app.port(18080).multithreaded().run();
+        std::cout << "Server starting on http://localhost:18080" << std::endl;
+        app.port(18080).multithreaded().run();
+
+    } catch (const std::exception& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cin.get();
+        return 1;
+    } catch (...) {
+        std::cerr << "Unknown error!" << std::endl;
+        std::cin.get();
+        return 1;
+    }
+
     return 0;
 }
